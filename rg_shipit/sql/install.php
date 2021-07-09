91f14a285a8cdaaefa1467bb391e84c04972f033
david25848 <davidmaring1997@gmail.com>
2021-07-09 12:25:06 -0400

    Improvement: Adding new statuses to order for update and update statuses via webhook

--- rg_shipit/sql/install.php
+++ rg_shipit/sql/install.php
@@ -47,6 +47,45 @@ $sql[] = 'CREATE TABLE IF NOT EXISTS `'._DB_PREFIX_.'rg_shipit_shipment` (
     PRIMARY KEY  (`id_rg_shipit_shipment`)
 ) ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8;';
 
+$sql[] = "INSERT INTO `'._DB_PREFIX_.'ps_order_state` (
+    `id_order_state`,
+    `invoice`, 
+    `send_email`, 
+    `module_name`, 
+    `color`, 
+    `unremovable`, 
+    `hidden`, 
+    `logable`, 
+    `delivery`, 
+    `shipped`, 
+    `paid`, 
+    `pdf_invoice`, 
+    `pdf_delivery`, 
+    `deleted`) 
+    VALUES  (14, 0, 0, '', '#f4cf58', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (15, 0, 0, '', '#6cca79', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (16, 0, 0, '', '#f4cf58', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (17, 0, 0, '', '#f4cf58', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (18, 0, 0, '', '#dd7272', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (19, 0, 0, '', '#e6b8af', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (20, 0, 0, '', '#484a7d', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (21, 0, 0, '', '#f4cf58', 1, 0, 0, 1, 1, 1, 0, 0, 0),
+            (22, 0, 0, '', '#484a7d', 1, 0, 0, 1, 1, 1, 0, 0, 0);";
+
+$sql[] = "INSERT INTO `'._DB_PREFIX_.'ps_order_state_lang` (
+    `id_order_state`, 
+    `id_lang`, `name`, 
+    `template`)
+    VALUES  (14, 1, 'En ruta', ''),
+            (15, 1, 'Disponible en sucursal', ''),
+            (16, 1, 'Problemas de transporte', ''),
+            (17, 1, 'Problemas de dirección', ''),
+            (18, 1, 'Reembolsado por Shipit', ''),
+            (19, 1, 'Problemas de transporte', ''),
+            (20, 1, 'Devuelto', ''),
+            (21, 1, 'En tránsito', ''),
+            (22, 1, 'Envío cancelado', '');";
+            
 foreach ($sql as $query) {
     if (Db::getInstance()->execute($query) == false) {
         return false;
