8aa7ef46abe320ebaf8759dd1cfbb926ba75a733
David Marin <davidmaring1997@gmail.com>
2021-08-18 14:46:20 -0400

    Reference: #633ye2 Improvement: Adding class for request to boxify

---
+++ rg_shipit/shipit_service/ShipitBoxify.php
@@ -0,0 +1,28 @@
+<?php
+  class ShipitBoxify {
+    public $url = '';
+    public $email = '';
+    public $token = '';
+    public $headers = '';
+    public $base = '';
+
+    public function __construct() {
+      $this->base = 'http://boxify.shipit.cl';
+      $this->headers = array(
+        'Content-Type' => 'application/json'
+      );
+    }
+
+    function calculate($shipment = array()) {
+      $client = new HttpClient($this->base . '/packs', $this->headers);
+      $response = $client->post($shipment);
+      $data = array();
+      if (is_wp_error($response)) {
+        echo 'Error al conectar con API.';
+      } else {
+        $data = json_decode($response['body']);
+      }
+      return (array)$data->packing_measures;
+    }
+  }
+?>
